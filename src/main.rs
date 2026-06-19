038d92d9dc8fc4a1df52d92bb8c6bd1ace677ffc
Nolan Peterson <ndpete2007@gmail.com>
2026-06-19 11:23:23 -0500

    feat: udp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello world");
+use std::{net::SocketAddr, sync::Arc};
+
+use tokio::{net::UdpSocket, sync::mpsc};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:53".parse::<SocketAddr>()?).await?;
+    let r = Arc::new(socket);
+    let s = r.clone();
+
+    let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
+
+    tokio::spawn(async move {
+        while let Some((bytes, addr)) = rx.recv().await {
+            let len = s.send_to(&bytes, &addr).await.unwrap();
+            println!("{:?} bytes sent", len);
+        }
+    });
+
+    let mut buf = [0; 1024];
+    loop {
+        let (len, addr) = r.recv_from(&mut buf).await?;
+        println!("{:?} bytes received from {:?}", len, addr);
+        tx.send((buf[..len].to_vec(), addr)).await.unwrap();
+    }
+
+    Ok(())
 }
