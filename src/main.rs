1ed2ff23d619c8dbe5f2f2f86811b5de7a1054bd
Nolan Peterson <ndpete2007@gmail.com>
2026-06-19 12:18:06 -0500

    feat: dns struct implementation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,38 @@ use std::{net::SocketAddr, sync::Arc};
 
 use tokio::{net::UdpSocket, sync::mpsc};
 
+struct Header {
+    id: u16,
+    /// Query(true) or Response(false)
+    qr: bool,
+    opcode: u8, // 4 bits
+    aa: bool,
+    tc: bool,
+    rd: bool,
+    ra: bool,
+    z: u8, // 3 bits, reserved for future use
+    rcode: u8,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
+struct Question {
+    qname: String, // Each label has a u8 for length of label. 0x003 0x777777 = www
+    qtype: u16,
+    qclass: u16,
+}
+
+struct Answer {
+    name: String, // Each label has a u8 for length of label. 0x003 0x777777 = www
+    r#type: u16,
+    class: u16,
+    ttl: u32,
+    rdlength: u16,
+    rdata: Vec<u8>, // depends on type field
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:53".parse::<SocketAddr>()?).await?;
@@ -20,7 +52,11 @@ async fn main() -> anyhow::Result<()> {
     let mut buf = [0; 1024];
     loop {
         let (len, addr) = r.recv_from(&mut buf).await?;
+
         println!("{:?} bytes received from {:?}", len, addr);
+        println!("raw: {:?}", buf);
+        println!("utf8 lossy: {}", String::from_utf8_lossy(&buf));
+
         tx.send((buf[..len].to_vec(), addr)).await.unwrap();
     }
 
